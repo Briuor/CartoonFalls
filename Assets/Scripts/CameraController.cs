b43bccadd4f1a6f6bc8e3e16a193e4e5d5c7c80a
Andre Mury <mury_gh@hotmail.com>
2019-09-12 19:35:22 -0300

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public bool isJumping = false;
+    private bool isGrounded;
+    public float jumpForce = 10f;
+    private float moveInput;
+    private Collider2D playerCollider;
+    private Rigidbody2D rb;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask ground;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        isGrounded = false;
+        playerCollider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, ground);
+        moveInput = Input.GetAxis("Horizontal") * speed;
+
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        rb.velocity = new Vector2(moveInput, rb.velocity.y);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+    }
+
+}
